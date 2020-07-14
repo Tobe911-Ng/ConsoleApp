7c13b76132a85476cc76aae71978265728650465
Tobe911-Ng <59968993+Tobe911-Ng@users.noreply.github.com>
2020-07-14 09:45:06 -0700

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ConsoleApp1.Services;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Class1.GetName();
+            var age = Class1.GetAge();
+            Console.WriteLine("Name:{0}\nAge: {1}", name, age);
+            Console.WriteLine("My name is {0}, and I am {1} years old", name, age);
+        }
+         
+    }
+
+}
+
+
---
+++ ConsoleApp1/Services/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Services
+{
+    class Class1
+    {
+        public static string GetName(){
+            Console.WriteLine("Enter your name");
+            string getName = Console.ReadLine();
+            return getName;
+        }
+        public static int GetAge(){
+            int age = 0;
+
+            Console.WriteLine("Enter your Date of Birth (DD/MM/YYYY)");
+            string getDate = Console.ReadLine();
+
+            int currentYear = DateTime.Now.Year;
+            int currentMonth = DateTime.Now.Month;
+            int currentDay = DateTime.Now.Day;
+
+            try
+            {
+                DateTime dob = Convert.ToDateTime(getDate);
+                int birthYear = dob.Year;
+                int birthMonth = dob.Month;
+                int birthDay = dob.Day;
+                age = currentYear - birthYear;
+                if (currentMonth < birthMonth)
+                {
+                    --age;
+                }
+                else if (currentMonth == birthMonth)
+                {
+                    if(currentDay > birthDay)
+                    {
+                        --age;
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Incorrect date format");
+            }
+
+            return age;
+        }
+    }
+}
